f89fca8b14e2ed0f8d060c58d4d15eaa1fe25982
Chukwudi Eze <chukwudieze97@gmail.com>
2022-12-02 10:58:15 +0100

    create user counts

--- src/components/stats/user-counts.jsx
+++ src/components/stats/user-counts.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const UserCounts = () => {
   return (
-    <div className='user-counts'>
-        <div>
-            <p>daily active user</p>
-            <h4></h4>
-        </div>
+    <div className="user-counts">
+      <div>
+        <p>daily active user</p>
+        <h4>10</h4>
+      </div>
+      <div>
+        <p>weekly active user</p>
+        <h4>300</h4>
+      </div>
+      <div>
+        <p>monthly active user</p>
+        <h4>1000</h4>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserCounts
+export default UserCounts;
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StatsGraph from "../../components/stats/stats-graph";
+import UserCounts from "../../components/stats/user-counts";
 
 const Dashboard = () => {
   return (
@@ -7,6 +8,9 @@ const Dashboard = () => {
       <div className="graph-wrapper">
         <StatsGraph title="Total number of server" amount="1,926" />
         <StatsGraph title="total users" amount="3,142" />
+      </div>
+      <div className="user-counts-wrapper">
+        <UserCounts />
       </div>
     </div>
   );
