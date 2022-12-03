f2f387494c047b0446f32d1b9f6b54f8fcec767a
Chukwudi Eze <chukwudieze97@gmail.com>
2022-12-03 12:10:30 +0100

    create table

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -13,18 +13,81 @@ const Dashboard = () => {
       <div className="user-counts-wrapper">
         <UserCounts />
       </div>
-      <div className="server-table">
-        <div>
-          <Pagination
-            currentPage={1}
-            maxPageLimit={5}
-            minPageLimit={0}
-            totalPages={16}
-            onPrevClick={() => null}
-            onNextClick={() => null}
-            onPageChange={() => null}
-          />
+      <div className="server-table-wrapper">
+        <h4 className="table-title">Servers</h4>
+        <div className="server-table">
+          <div className="table-header">
+            <div>Name</div>
+            <div>member</div>
+            <div>type</div>
+            <div>online</div>
+          </div>
+          <div className="table-row">
+            <div>ZaQ.GameZone</div>
+            <div>1,025</div>
+            <div>Game guild</div>
+            <div>172</div>
+          </div>
+          <div className="table-row">
+            <div>ZaQ.GameZone</div>
+            <div>1,025</div>
+            <div>Game guild</div>
+            <div>172</div>
+          </div>
+          <div className="table-row">
+            <div>ZaQ.GameZone</div>
+            <div>1,025</div>
+            <div>Game guild</div>
+            <div>172</div>
+          </div>
+          <div className="table-row">
+            <div>ZaQ.GameZone</div>
+            <div>1,025</div>
+            <div>Game guild</div>
+            <div>172</div>
+          </div>
+          <div className="table-row">
+            <div>ZaQ.GameZone</div>
+            <div>1,025</div>
+            <div>Game guild</div>
+            <div>172</div>
+          </div>
+          <div className="table-row">
+            <div>ZaQ.GameZone</div>
+            <div>1,025</div>
+            <div>Game guild</div>
+            <div>172</div>
+          </div>
+          <div className="table-row">
+            <div>ZaQ.GameZone</div>
+            <div>1,025</div>
+            <div>Game guild</div>
+            <div>172</div>
+          </div>
+          <div className="table-row">
+            <div>ZaQ.GameZone</div>
+            <div>1,025</div>
+            <div>Game guild</div>
+            <div>172</div>
+          </div>
+          <div className="table-row">
+            <div>ZaQ.GameZone</div>
+            <div>1,025</div>
+            <div>Game guild</div>
+            <div>172</div>
+          </div>
         </div>
+      </div>
+      <div>
+        <Pagination
+          currentPage={1}
+          maxPageLimit={5}
+          minPageLimit={0}
+          totalPages={16}
+          onPrevClick={() => null}
+          onNextClick={() => null}
+          onPageChange={() => null}
+        />
       </div>
     </div>
   );
